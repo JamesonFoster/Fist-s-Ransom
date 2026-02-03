70a721d256995be96b39241d58be9d879fdabda3
JamesonFoster <jamesonbfoster@gmail.com>
2026-02-02 18:03:55 -0600

    Imported Work into Project

---
+++ Fist's Ransom/Assets/Scripts/PlayerAtk.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAtk : MonoBehaviour
+{
+    public EnemyMovement target;
+    private PlayerMovement plMove;
+
+    public bool aimUp = false; // Holding W aims punches upward
+
+    public float atkCooldown = 0.05f;
+    private float timeratkCooldown = 0f;
+
+    public bool isAtking = false;
+
+    private Vector2 startPos;
+    private Vector2 attackPos;
+
+
+
+    void Awake()
+    {
+        plMove = GetComponent<PlayerMovement>();
+    }
+    void Start()
+    {
+        startPos = transform.position;
+        Vector2 attackPos = new Vector2(0, 0);
+    }
+
+    void Update()
+    {
+        // Aim up check
+        aimUp = Input.GetKey(KeyCode.W);
+
+        // Attack input
+        if (!isAtking && timeratkCooldown <= 0f && !plMove.isDodging)
+        {
+            if (Input.GetKeyDown(KeyCode.Comma))
+                AttackL();
+
+            if (Input.GetKeyDown(KeyCode.Period))
+                AttackR();
+        }
+
+        // Attack movement
+        if (isAtking)
+        {
+            timeratkCooldown += Time.deltaTime;
+            if (timeratkCooldown <= atkCooldown / 2f)
+            {
+                // Move outward
+                transform.position = Vector2.MoveTowards(transform.position,attackPos,10f * Time.deltaTime);
+            }
+            else if (timeratkCooldown <= atkCooldown)
+            {
+                // Move back
+                transform.position = Vector2.MoveTowards(transform.position,startPos,10f * Time.deltaTime);
+            }
+            else
+            {
+                // Reset
+                isAtking = false;
+                timeratkCooldown = 0f;
+                transform.position = startPos;
+            }
+        }
+    }
+
+    public void AttackL()
+    {
+        isAtking = true;
+
+        if (aimUp)
+            SendScore(target, "headL");
+        else
+            SendScore(target, "bodyL");
+    }
+
+    public void AttackR()
+    {
+        isAtking = true;
+
+        if (aimUp)
+            SendScore(target, "headR");
+        else
+            SendScore(target, "bodyR");
+    }
+
+    public void SendScore(EnemyMovement target, string dir)
+    {
+        if (target != null)
+        {
+            target.ReceiveScore(dir);
+        }
+        else
+        {
+            Debug.LogWarning("Target is missing!");
+        }
+    }
+    
+}
---
+++ Fist's Ransom/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float dodgeDistance = 5f; //Distance the Player moves when dodging
+    public float dodgeTime = 0.4f; //The time it takes for a player to no longer be dodging
+    public float dodgeStun = 0.1f; //The time after a complete dodge where a player can't dodge again
+
+    public bool isDodging = false; //Controls if player is dodging
+    private float dodgeTimer = 0f; //A tester value to test how long the player has been dodging
+    private float stunTimer = 999f; //A tester value to test how long the player has been dodge stunned
+    private Vector2 dodgeTarget; //The target position the player moves to when dodging
+    private Vector2 startPos; //The starting posistion / the position the player returns to after dodging
+
+    private PlayerAtk plAtk;
+
+
+    
+    void Awake()
+    {
+        plAtk = GetComponent<PlayerAtk>();
+    }
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        stunTimer += Time.deltaTime;
+        if (!isDodging && ((dodgeStun + dodgeTime) < stunTimer && !plAtk.isAtking)) //Allows dodging only if player isn't dodging or atk
+        {                                                       //and if dodge stun isn't active
+            if (Input.GetKeyDown(KeyCode.A))
+                StartDodge(Vector2.left); 
+
+            if (Input.GetKeyDown(KeyCode.D))
+                StartDodge(Vector2.right);
+
+            if (Input.GetKeyDown(KeyCode.S))
+                StartDodge(Vector2.down);
+        }
+        else
+        {
+            dodgeTimer += Time.deltaTime;
+            
+            if (dodgeTimer <= dodgeTime / 2f)
+            {
+                // Move outward
+                transform.position = Vector2.MoveTowards(transform.position,dodgeTarget,(dodgeDistance / (dodgeTime / 2f)) * Time.deltaTime);
+            }
+            else if (dodgeTimer <= dodgeTime)
+            {
+                // Move back
+                transform.position = Vector2.MoveTowards(transform.position,startPos,(dodgeDistance / (dodgeTime / 2f)) * Time.deltaTime);
+            }
+            else
+            {
+                isDodging = false;
+                transform.position = startPos; // snap cleanly
+            }
+        }
+    }
+
+    void StartDodge(Vector2 direction) // A function used to set up the values for dodging
+    {
+        isDodging = true;
+        dodgeTimer = 0f;
+        stunTimer = 0f;
+        dodgeTarget = (Vector2)transform.position + direction * dodgeDistance;
+    }
+}
