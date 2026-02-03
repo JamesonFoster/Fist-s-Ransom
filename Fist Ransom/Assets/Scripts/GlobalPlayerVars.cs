dfc3367c622626b3d4f6b7ae394ff270365455d5
JamesonFoster <jamesonbfoster@gmail.com>
2026-02-02 18:52:19 -0600

    Added Scripts Back in

---
+++ Fist Ransom/Assets/Scripts/GlobalPlayerVars.cs
@@ -0,0 +1,11 @@
+using UnityEditor.PackageManager;
+using UnityEngine;
+
+public class GlobalPlayerVars : MonoBehaviour
+{
+    public static int HealCount = 3;
+    public static int RageCount = 1;
+    public static float PlayerHealth = 50;
+    public static float PlayerMaxHealth = 100;
+    public static int PlayerRage = 0;
+}
