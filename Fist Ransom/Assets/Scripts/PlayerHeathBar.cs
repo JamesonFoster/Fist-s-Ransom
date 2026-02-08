eda566f5a5ada6cd497d35d3a3cbec3d6df023af
JamesonFoster <jamesonbfoster@gmail.com>
2026-02-08 14:29:53 -0600

    Ready For OverRide

--- Fist Ransom/Assets/Scripts/PlayerHeathBar.cs
+++ Fist Ransom/Assets/Scripts/PlayerHeathBar.cs
@@ -1,24 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerHeathBar : MonoBehaviour
 {
-    private Vector3 cubStr;
-    private Vector3 cubStrOg;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private Slider _slider;
+    private float _health = 100;
+    // Start is called before the first frame update
     void Start()
     {
         float healpercent = (GlobalPlayerVars.PlayerMaxHealth / GlobalPlayerVars.PlayerHealth) * 100;
-        cubStrOg = transform.localScale;
-        cubStr = transform.localScale;
+        _slider.value = healpercent;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float healpercent = (GlobalPlayerVars.PlayerHealth / GlobalPlayerVars.PlayerMaxHealth);
-
-        cubStr.x = cubStrOg.x * healpercent;
-
-        transform.localScale = cubStr;
+        float healpercent = (GlobalPlayerVars.PlayerMaxHealth / GlobalPlayerVars.PlayerHealth) * 100;
+        if (Input.GetKeyUp(KeyCode.Q))
+        {
+            if (healpercent > 0)
+            {
+                healpercent -= 20f;
+                _slider.value = healpercent;
+            }
+        }
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            if (healpercent <= 99)
+            {
+                healpercent += 20f;
+                _slider.value = healpercent;
+            }
+        }
     }
 }
